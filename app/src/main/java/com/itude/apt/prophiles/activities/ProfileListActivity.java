a765ec70596055e655559af5871a87065764e460
Alexander Thompson <athompson@128technology.com>
2017-01-14 13:49:43 -0500

    Cleaned up ProfileListActivity

--- app/src/main/java/com/itude/apt/prophiles/activities/ProfileListActivity.java
+++ app/src/main/java/com/itude/apt/prophiles/activities/ProfileListActivity.java
@@ -3,7 +3,6 @@ package com.itude.apt.prophiles.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,19 +24,29 @@ public class ProfileListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_list);
 
+        setUpActionBar();
+        setUpRecyclerView();
+        setUpFloatingActionButton();
+    }
+
+    private void setUpActionBar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        RecyclerView profileListView = (RecyclerView) findViewById(R.id.content_main_profile_list);
-        profileListView.setHasFixedSize(true);
+    private void setUpRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.content_main_profile_list);
+        recyclerView.setHasFixedSize(true);
 
-        RecyclerView.LayoutManager profileListLayoutManager = new LinearLayoutManager(this);
-        profileListView.setLayoutManager(profileListLayoutManager);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
         List<Profile> profiles = Profile.listAll(Profile.class);
         mAdapter = new ProfileListAdapter(profiles);
-        profileListView.setAdapter(mAdapter);
+        recyclerView.setAdapter(mAdapter);
+    }
 
+    private void setUpFloatingActionButton() {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.create_profile_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
