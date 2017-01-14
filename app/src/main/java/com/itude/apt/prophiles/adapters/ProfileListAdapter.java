14a69e38f1085039e26d3e792ca76282ec1b3f74
Alexander Thompson <athompson@128technology.com>
2017-01-14 14:37:06 -0500

    Added click listener to ProfileListAdapter.ViewHolder

--- app/src/main/java/com/itude/apt/prophiles/adapters/ProfileListAdapter.java
+++ app/src/main/java/com/itude/apt/prophiles/adapters/ProfileListAdapter.java
@@ -1,10 +1,13 @@
 package com.itude.apt.prophiles.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.itude.apt.prophiles.model.Profile;
 
@@ -18,16 +21,30 @@ public class ProfileListAdapter extends RecyclerView.Adapter<ProfileListAdapter.
 
     private List<Profile> mProfiles;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Context mContext;
+        private Profile mProfile;
         private TextView mNameTextView;
 
         public ViewHolder(View profileView) {
             super(profileView);
+            mContext = profileView.getContext();
             mNameTextView = (TextView) profileView.findViewById(android.R.id.text1);
         }
 
         public void bindProfile(Profile profile) {
+            mProfile = profile;
             mNameTextView.setText(profile.name);
+            mNameTextView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(
+                mContext,
+                "Selected profile " + mProfile.name,
+                Toast.LENGTH_SHORT
+            ).show();
         }
     }
 
